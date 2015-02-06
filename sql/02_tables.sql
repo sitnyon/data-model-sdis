11e0c6ca992fbd3be95f0fb8ca48d21acc3b4dd3
Rémi Bovard <remi.bovard@nyon.ch>
2015-02-06 12:06:49 +0100

    Create table risques

--- sql/02_tables.sql
+++ sql/02_tables.sql
@@ -12,3 +12,20 @@ ALTER TABLE sdis.dangers ADD COLUMN remarque     varchar(255);
 
 SELECT AddGeometryColumn('sdis', 'dangers', 'geom', 21781, 'Point', 2);
 CREATE INDEX dangers_geom_idx ON sdis.dangers USING GIST (geom);
+
+
+
+/* TABLE risques */
+
+--DROP TABLE IF EXISTS sdis.risques CASCADE;
+CREATE TABLE sdis.risques (id serial PRIMARY KEY);
+COMMENT ON TABLE sdis.risques IS 'Risques';
+
+ALTER TABLE sdis.risques ADD COLUMN type         varchar(255); COMMENT ON COLUMN sdis.risques.type IS 'EMS, Hôpital, Ecole, Parking souterrain, Bien culturel, Pisciculture, Bâtiment isolé, Autre';
+ALTER TABLE sdis.risques ADD COLUMN localisation varchar(255);
+ALTER TABLE sdis.risques ADD COLUMN photo        varchar(255);
+ALTER TABLE sdis.risques ADD COLUMN document     varchar(255);
+ALTER TABLE sdis.risques ADD COLUMN remarque     varchar(255);
+
+SELECT AddGeometryColumn('sdis', 'risques', 'geom', 21781, 'Point', 2);
+CREATE INDEX risques_geom_idx ON sdis.risques USING GIST (geom);
