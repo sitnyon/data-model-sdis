72f865dcc82f9aba9cb4ba669acced8e9a8d51b2
Rémi Bovard <remi.bovard@nyon.ch>
2015-07-13 16:32:06 +0200

    Add comment about specific Nyon

--- sql/03_views.sql
+++ sql/03_views.sql
@@ -5,7 +5,7 @@ CREATE VIEW sdis.batiments AS
 
     -- Bâtiments MO
     SELECT DISTINCT b.gid AS id, b.numero AS no_eca, b.commune AS commune, b.geom
-    FROM mo.mo_baths b
+    FROM mo.mo_baths b -- Spécifique Nyon
     JOIN (
         SELECT geom FROM sdis.dangers               UNION
         SELECT geom FROM sdis.risques               UNION
@@ -20,7 +20,7 @@ CREATE VIEW sdis.batiments AS
 
     -- Cadastre projeté
     SELECT DISTINCT b.fid + 100000 AS id, 'Projet' AS no_eca, b.commune AS commune, b.geom
-    FROM cad.cad_va_bat_hs_comm b
+    FROM cad.cad_va_bat_hs_comm b -- Spécifique Nyon
     JOIN (
         SELECT geom FROM sdis.dangers               UNION
         SELECT geom FROM sdis.risques               UNION
