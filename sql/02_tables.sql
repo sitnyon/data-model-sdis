f3f851c3a051f71a0abf3435bcc2cfec77a4ea85
Rémi Bovard <remi.bovard@nyon.ch>
2015-07-14 08:59:42 +0200

    Use new way to create geom columns

--- sql/02_tables.sql
+++ sql/02_tables.sql
@@ -10,8 +10,8 @@ ALTER TABLE sdis.dangers ADD COLUMN photo        varchar(255);
 ALTER TABLE sdis.dangers ADD COLUMN document     varchar(255);
 ALTER TABLE sdis.dangers ADD COLUMN remarque     varchar(255);
 ALTER TABLE sdis.dangers ADD COLUMN date_import  date;
+ALTER TABLE sdis.dangers ADD COLUMN geom         geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'dangers', 'geom', 21781, 'Point', 2);
 CREATE INDEX dangers_geom_idx ON sdis.dangers USING GIST (geom);
 
 
@@ -28,8 +28,8 @@ ALTER TABLE sdis.risques ADD COLUMN photo        varchar(255);
 ALTER TABLE sdis.risques ADD COLUMN document     varchar(255);
 ALTER TABLE sdis.risques ADD COLUMN remarque     varchar(255);
 ALTER TABLE sdis.risques ADD COLUMN date_import  date;
+ALTER TABLE sdis.risques ADD COLUMN geom         geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'risques', 'geom', 21781, 'Point', 2);
 CREATE INDEX risques_geom_idx ON sdis.risques USING GIST (geom);
 
 
@@ -44,8 +44,8 @@ ALTER TABLE sdis.acces ADD COLUMN type         varchar(255) NOT NULL; COMMENT ON
 ALTER TABLE sdis.acces ADD COLUMN localisation varchar(255);
 ALTER TABLE sdis.acces ADD COLUMN photo        varchar(255);
 ALTER TABLE sdis.acces ADD COLUMN remarque     varchar(255);
+ALTER TABLE sdis.acces ADD COLUMN geom         geometry(MultiLineString, 21781);
 
-SELECT AddGeometryColumn('sdis', 'acces', 'geom', 21781, 'MultiLineString', 2);
 CREATE INDEX acces_geom_idx ON sdis.acces USING GIST (geom);
 
 
@@ -60,8 +60,8 @@ ALTER TABLE sdis.signalisation ADD COLUMN type        varchar(255) NOT NULL; COM
 ALTER TABLE sdis.signalisation ADD COLUMN valeur      varchar(5);            COMMENT ON COLUMN sdis.signalisation.valeur IS 'Hauteur, largeur ou poids maximal';
 ALTER TABLE sdis.signalisation ADD COLUMN remarque    varchar(255);
 ALTER TABLE sdis.signalisation ADD COLUMN orientation int4;                  COMMENT ON COLUMN sdis.signalisation.orientation IS '[°]';
+ALTER TABLE sdis.signalisation ADD COLUMN geom        geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'signalisation', 'geom', 21781, 'Point', 2);
 CREATE INDEX signalisation_geom_idx ON sdis.signalisation USING GIST (geom);
 
 
@@ -74,8 +74,8 @@ COMMENT ON TABLE sdis.emplacements IS 'Emplacements pour moyens d''intervention'
 
 ALTER TABLE sdis.emplacements ADD COLUMN type     varchar(255) NOT NULL; COMMENT ON COLUMN sdis.emplacements.type IS 'Motopompe, Tonne pompe, Echelle';
 ALTER TABLE sdis.emplacements ADD COLUMN remarque varchar(255);
+ALTER TABLE sdis.emplacements ADD COLUMN geom     geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'emplacements', 'geom', 21781, 'Point', 2);
 CREATE INDEX emplacements_geom_idx ON sdis.emplacements USING GIST (geom);
 
 
@@ -92,8 +92,8 @@ ALTER TABLE sdis.detections ADD COLUMN gaz          bool;
 ALTER TABLE sdis.detections ADD COLUMN sprinkler    bool;
 ALTER TABLE sdis.detections ADD COLUMN localisation varchar(255);
 ALTER TABLE sdis.detections ADD COLUMN remarque     varchar(255);
+ALTER TABLE sdis.detections ADD COLUMN geom         geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'detections', 'geom', 21781, 'Point', 2);
 CREATE INDEX detections_geom_idx ON sdis.detections USING GIST (geom);
 
 
@@ -110,8 +110,8 @@ ALTER TABLE sdis.cles ADD COLUMN localisation  varchar(255);
 ALTER TABLE sdis.cles ADD COLUMN remarque      varchar(255);
 ALTER TABLE sdis.cles ADD COLUMN date_controle date;
 ALTER TABLE sdis.cles ADD COLUMN inactif       bool;
+ALTER TABLE sdis.cles ADD COLUMN geom          geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'cles', 'geom', 21781, 'Point', 2);
 CREATE INDEX cles_geom_idx ON sdis.cles USING GIST (geom);
 
 
@@ -126,8 +126,8 @@ ALTER TABLE sdis.codes ADD COLUMN code          varchar(255) NOT NULL;
 ALTER TABLE sdis.codes ADD COLUMN localisation  varchar(255);
 ALTER TABLE sdis.codes ADD COLUMN date_controle date;
 ALTER TABLE sdis.codes ADD COLUMN remarque      varchar(255);
+ALTER TABLE sdis.codes ADD COLUMN geom          geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'codes', 'geom', 21781, 'Point', 2);
 CREATE INDEX codes_geom_idx ON sdis.codes USING GIST (geom);
 
 
@@ -141,8 +141,8 @@ COMMENT ON TABLE sdis.situation IS 'Eléments de situation';
 ALTER TABLE sdis.situation ADD COLUMN type         varchar(255) NOT NULL; COMMENT ON COLUMN sdis.situation.type IS 'Ascenseur';
 ALTER TABLE sdis.situation ADD COLUMN localisation varchar(255);
 ALTER TABLE sdis.situation ADD COLUMN remarque     varchar(255);
+ALTER TABLE sdis.situation ADD COLUMN geom         geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'situation', 'geom', 21781, 'Point', 2);
 CREATE INDEX situation_geom_idx ON sdis.situation USING GIST (geom);
 
 
@@ -157,8 +157,8 @@ ALTER TABLE sdis.dossiers_intervention ADD COLUMN document     varchar(255) NOT 
 ALTER TABLE sdis.dossiers_intervention ADD COLUMN localisation varchar(255);
 ALTER TABLE sdis.dossiers_intervention ADD COLUMN statut       varchar(255);          COMMENT ON COLUMN sdis.dossiers_intervention.statut IS 'En vigueur, Provisoire, A modifier';
 ALTER TABLE sdis.dossiers_intervention ADD COLUMN remarque     varchar(255);
+ALTER TABLE sdis.dossiers_intervention ADD COLUMN geom         geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'dossiers_intervention', 'geom', 21781, 'Point', 2);
 CREATE INDEX dossiers_intervention_geom_idx ON sdis.dossiers_intervention USING GIST (geom);
 
 
@@ -175,8 +175,8 @@ ALTER TABLE sdis.bornes_hydrantes ADD COLUMN sortie   varchar(255);          COM
 ALTER TABLE sdis.bornes_hydrantes ADD COLUMN pression float8;                COMMENT ON COLUMN sdis.bornes_hydrantes.pression IS '[bar]';
 ALTER TABLE sdis.bornes_hydrantes ADD COLUMN debit    int4;                  COMMENT ON COLUMN sdis.bornes_hydrantes.debit IS '[l/min]';
 ALTER TABLE sdis.bornes_hydrantes ADD COLUMN remarque varchar(255);
+ALTER TABLE sdis.bornes_hydrantes ADD COLUMN geom     geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'bornes_hydrantes', 'geom', 21781, 'Point', 2);
 CREATE INDEX bornes_hydrantes_geom_idx ON sdis.bornes_hydrantes USING GIST (geom);
 
 
@@ -191,8 +191,8 @@ ALTER TABLE sdis.puits_reservoirs ADD COLUMN type     varchar(255) NOT NULL; COM
 ALTER TABLE sdis.puits_reservoirs ADD COLUMN capacite int4;                  COMMENT ON COLUMN sdis.puits_reservoirs.capacite IS '[m3]';
 ALTER TABLE sdis.puits_reservoirs ADD COLUMN reserve  int4;                  COMMENT ON COLUMN sdis.puits_reservoirs.reserve IS '[m3]';
 ALTER TABLE sdis.puits_reservoirs ADD COLUMN remarque varchar(255);
+ALTER TABLE sdis.puits_reservoirs ADD COLUMN geom     geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'puits_reservoirs', 'geom', 21781, 'Point', 2);
 CREATE INDEX puits_reservoirs_geom_idx ON sdis.puits_reservoirs USING GIST (geom);
 
 
@@ -206,8 +206,8 @@ COMMENT ON TABLE sdis.tuyaux IS 'Emplacements pour tuyaux';
 ALTER TABLE sdis.tuyaux ADD COLUMN diametre varchar(255); COMMENT ON COLUMN sdis.tuyaux.diametre IS '75 mm, 110 mm';
 ALTER TABLE sdis.tuyaux ADD COLUMN longueur int4;         COMMENT ON COLUMN sdis.tuyaux.longueur IS '[m]';
 ALTER TABLE sdis.tuyaux ADD COLUMN remarque varchar(255);
+ALTER TABLE sdis.tuyaux ADD COLUMN geom     geometry(MultiLineString, 21781);
 
-SELECT AddGeometryColumn('sdis', 'tuyaux', 'geom', 21781, 'MultiLineString', 2);
 CREATE INDEX tuyaux_geom_idx ON sdis.tuyaux USING GIST (geom);
 
 
@@ -222,8 +222,8 @@ ALTER TABLE sdis.barrages ADD COLUMN type         varchar(255) NOT NULL; COMMENT
 ALTER TABLE sdis.barrages ADD COLUMN longueur     int4;                  COMMENT ON COLUMN sdis.barrages.longueur IS '[m]';
 ALTER TABLE sdis.barrages ADD COLUMN localisation varchar(255);
 ALTER TABLE sdis.barrages ADD COLUMN remarque     varchar(255);
+ALTER TABLE sdis.barrages ADD COLUMN geom         geometry(MultiLineString, 21781);
 
-SELECT AddGeometryColumn('sdis', 'barrages', 'geom', 21781, 'MultiLineString', 2);
 CREATE INDEX barrages_geom_idx ON sdis.barrages USING GIST (geom);
 
 
@@ -237,8 +237,8 @@ COMMENT ON TABLE sdis.sites IS 'Casernes et locaux';
 ALTER TABLE sdis.sites ADD COLUMN type      varchar(255) NOT NULL; COMMENT ON COLUMN sdis.sites.type IS 'DPS, DAP';
 ALTER TABLE sdis.sites ADD COLUMN categorie varchar(5);
 ALTER TABLE sdis.sites ADD COLUMN nom       varchar(255) NOT NULL;
+ALTER TABLE sdis.sites ADD COLUMN geom      geometry(Point, 21781);
 
-SELECT AddGeometryColumn('sdis', 'sites', 'geom', 21781, 'Point', 2);
 CREATE INDEX sites_geom_idx ON sdis.sites USING GIST (geom);
 
 
@@ -251,8 +251,8 @@ COMMENT ON TABLE sdis.secteurs_intervention IS 'Secteurs d''intervention';
 
 ALTER TABLE sdis.secteurs_intervention ADD COLUMN type      varchar(255) NOT NULL; COMMENT ON COLUMN sdis.secteurs_intervention.type IS 'Feu, Désincarcération, DCH';
 ALTER TABLE sdis.secteurs_intervention ADD COLUMN sous_type varchar(255);          COMMENT ON COLUMN sdis.secteurs_intervention.sous_type IS 'Feu primaire, Feu secondaire, Feu renfort';
+ALTER TABLE sdis.secteurs_intervention ADD COLUMN geom      geometry(MultiPolygon, 21781);
 
-SELECT AddGeometryColumn('sdis', 'secteurs_intervention', 'geom', 21781, 'MultiPolygon', 2);
 CREATE INDEX secteurs_intervention_geom_idx ON sdis.secteurs_intervention USING GIST (geom);
 
 
@@ -265,8 +265,8 @@ COMMENT ON TABLE sdis.secteurs_organisationnels IS 'Secteurs organisationnels';
 
 ALTER TABLE sdis.secteurs_organisationnels ADD COLUMN type varchar(255) NOT NULL;
 ALTER TABLE sdis.secteurs_organisationnels ADD COLUMN nom  varchar(255);
+ALTER TABLE sdis.secteurs_organisationnels ADD COLUMN geom geometry(MultiPolygon, 21781);
 
-SELECT AddGeometryColumn('sdis', 'secteurs_organisationnels', 'geom', 21781, 'MultiPolygon', 2);
 CREATE INDEX secteurs_organisationnels_geom_idx ON sdis.secteurs_organisationnels USING GIST (geom);
 
 
