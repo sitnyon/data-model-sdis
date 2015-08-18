e9db385b42bc97a7bde738fe4526881fb69ff2b0
Rémi Bovard <remi.bovard@nyon.ch>
2015-08-18 15:46:07 +0200

    Also rename sequences (to avoid FME bug)

--- sql/nyon.sql
+++ sql/nyon.sql
@@ -1,4 +1,4 @@
--- Rename tables
+-- Rename tables (and sequences)
 ALTER TABLE sdis.acces                     RENAME TO sd_acces;
 ALTER TABLE sdis.barrages                  RENAME TO sd_barrages;
 ALTER TABLE sdis.bornes_hydrantes          RENAME TO sd_bornes_hydrantes;
@@ -17,6 +17,25 @@ ALTER TABLE sdis.signalisation             RENAME TO sd_signalisation;
 ALTER TABLE sdis.sites                     RENAME TO sd_sites;
 ALTER TABLE sdis.situation                 RENAME TO sd_situation;
 ALTER TABLE sdis.tuyaux                    RENAME TO sd_tuyaux;
+
+ALTER SEQUENCE sdis.acces_id_seq                     RENAME TO sd_acces_id_seq;
+ALTER SEQUENCE sdis.barrages_id_seq                  RENAME TO sd_barrages_id_seq;
+ALTER SEQUENCE sdis.bornes_hydrantes_id_seq          RENAME TO sd_bornes_hydrantes_id_seq;
+ALTER SEQUENCE sdis.cles_id_seq                      RENAME TO sd_cles_id_seq;
+ALTER SEQUENCE sdis.codes_id_seq                     RENAME TO sd_codes_id_seq;
+ALTER SEQUENCE sdis.dangers_id_seq                   RENAME TO sd_dangers_id_seq;
+ALTER SEQUENCE sdis.detections_id_seq                RENAME TO sd_detections_id_seq;
+ALTER SEQUENCE sdis.dossiers_intervention_id_seq     RENAME TO sd_dossiers_intervention_id_seq;
+ALTER SEQUENCE sdis.emplacements_id_seq              RENAME TO sd_emplacements_id_seq;
+ALTER SEQUENCE sdis.personnel_liste_id_seq           RENAME TO sd_personnel_liste_id_seq;
+ALTER SEQUENCE sdis.puits_reservoirs_id_seq          RENAME TO sd_puits_reservoirs_id_seq;
+ALTER SEQUENCE sdis.risques_id_seq                   RENAME TO sd_risques_id_seq;
+ALTER SEQUENCE sdis.secteurs_intervention_id_seq     RENAME TO sd_secteurs_intervention_id_seq;
+ALTER SEQUENCE sdis.secteurs_organisationnels_id_seq RENAME TO sd_secteurs_organisationnels_id_seq;
+ALTER SEQUENCE sdis.signalisation_id_seq             RENAME TO sd_signalisation_id_seq;
+ALTER SEQUENCE sdis.sites_id_seq                     RENAME TO sd_sites_id_seq;
+ALTER SEQUENCE sdis.situation_id_seq                 RENAME TO sd_situation_id_seq;
+ALTER SEQUENCE sdis.tuyaux_id_seq                    RENAME TO sd_tuyaux_id_seq;
 
 -- Rename views
 ALTER VIEW sdis.batiments RENAME TO sd_batiments;
